622af582d9ea9a50f1277f2c9af4f741b51f7f68
dragsec <draganyes@gmail.com>
2021-04-09 08:22:55 +0200

    intro

---
+++ myScript.js
@@ -0,0 +1,10 @@
+var anni = parseInt(prompt("Quanti è la tua età?"));
+var kmTot = parseInt(prompt("Quanti km vuoi percorrere?"));
+var prezzo = km * 0.21;
+var under18 = 0.8;
+var over65 =  0.6;
+if (age < 18){
+    var prezzo = prezzokm * under18;
+} else if (age > 65){
+    var prezzo = prezzokm * over65;
+}
